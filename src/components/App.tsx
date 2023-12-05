3716bc6ba02d5873c9979c3368aeb820ed77e3c6
seanclaybaugh <sdclaybaugh@gmail.com>
2023-12-05 11:27:27 -0800

    refactored query to useQuery for loading states and added header and styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -6,44 +6,54 @@ import {
   Text,
   View,
   FlatList,
+  ActivityIndicator,
 } from 'react-native';
-import {useApolloClient, gql} from '@apollo/client';
-import {AllStarshipsResponse, Starship} from '../types/starships';
+import {useQuery} from '@apollo/client';
+import {AllStarshipsResponse} from '../types/starships';
 import {ALL_STARSHIPS_QUERY} from '../queries/allStarships';
 
 const App = () => {
-  const apolloClient = useApolloClient();
-  const [starships, setStarships] = useState<Starship[]>([]);
-
-  apolloClient
-    .query<AllStarshipsResponse>({
-      query: gql`
-        ${ALL_STARSHIPS_QUERY}
-      `,
-    })
-    .then(({data}) => {
-      console.log(data.allStarships.starships);
-      setStarships(data.allStarships.starships);
-    });
+  const {loading, data} = useQuery<AllStarshipsResponse>(ALL_STARSHIPS_QUERY);
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <FlatList
-        data={starships}
-        renderItem={({item}) => (
-          <View>
-            <Text>{item.name}</Text>
-          </View>
-        )}
-      />
+      <StatusBar barStyle={'light-content'} />
+      <Text style={styles.header}>STAR WARS SPACESHIPS</Text>
+      {loading ? (
+        <ActivityIndicator
+          size={'large'}
+          color={'yellow'}
+          style={styles.loadingSpinner}
+        />
+      ) : (
+        <FlatList
+          data={data?.allStarships.starships}
+          renderItem={({item}) => (
+            <View>
+              <Text style={{color: 'yellow'}}>{item.name}</Text>
+            </View>
+          )}
+        />
+      )}
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: 'grey'},
+  container: {flex: 1, backgroundColor: 'black'},
+  header: {
+    alignSelf: 'center',
+    color: 'yellow',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
   starshipList: {},
+  loadingSpinner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default App;
